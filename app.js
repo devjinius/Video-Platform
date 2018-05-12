b935e51f3fbb17ab1fefd89ad4371b42720f6560
eugene94 <eugenekang94@gmail.com>
2018-05-12 14:09:03 +0900

    server작업

--- app.js
+++ app.js
@@ -8,33 +8,30 @@ const ExpressApp = express();
 const NextApp = next({
   dev, // 개발 모드인지 프로덕션인지에 대한 플래그 (true / false)
   quiet: dev,
-  dir: 'my-next-app-dir', // Next 프로젝트 파일이 위치한 디렉토리, 기본 값은 현재 디렉토리 값임('.')
   conf: { // next.config.js 에서 사용하는 객체 값
     webpack: {}
   }
 })
 const NextHandler = NextApp.getRequestHandler();
 
-const CustomRouter = require('./routes');
+// const CustomRouter = require('./routes');
 const NextRouter = express.Router();
 
-NextRouter.get('/route-a', (req, res) => NextApp.render(req, res, 'route-a', Object.assign({}, req.query, req.param)));
-NextRouter.get('/route-b', (req, res) => {
-  const result = {
-    'SOME RESULT': 'FROM DB'
-  };
-  res.result = result;
+// NextRouter.get('*', (req, res) => NextApp.render(req, res, '*', Object.assign({}, req.query, req.param)));
+// NextRouter.get('/route-b', (req, res) => {
+//   const result = {
+//     'SOME RESULT': 'FROM DB'
+//   };
+//   res.result = result;
 
-  return NextApp.render(req, res, 'route-b', Object.assign({}, req.query, req.param))
-});
+//   return NextApp.render(req, res, 'route-b', Object.assign({}, req.query, req.param))
+// });
 NextRouter.get('*', (req, res) => NextHandler(req, res));
 
 NextApp
   .prepare()
   .then(() => {
-    ExpressApp.use(/* SOME MIDDLEWARE 1 */);
-    ExpressApp.use(/* SOME MIDDLEWARE 2 */);
-    ExpressApp.use('/', CustomRouter);
+    // ExpressApp.use('/', CustomRouter);
 
     ExpressApp.use('/my-next-app', NextRouter);
 

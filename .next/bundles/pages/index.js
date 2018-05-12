aba1aa03f0d03fa72350831196d1902ac68aba1d
eugene94 <eugenekang94@gmail.com>
2018-05-12 13:04:34 +0900

    first-commit

---
+++ .next/bundles/pages/index.js
@@ -0,0 +1,26 @@
+module.exports =
+
+        __NEXT_REGISTER_PAGE('/', function() {
+          var comp = 
+      webpackJsonp([4],{
+
+/***/ "./pages/index.js":
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed: Error: [BABEL] C:\\Video-Platform\\pages\\index.js: .value is not a valid Plugin property\n    at C:\\Video-Platform\\node_modules\\@babel\\core\\lib\\config\\validation\\plugins.js:52:56\n    at Array.forEach (<anonymous>)\n    at validatePluginObject (C:\\Video-Platform\\node_modules\\@babel\\core\\lib\\config\\validation\\plugins.js:50:20)\n    at C:\\Video-Platform\\node_modules\\@babel\\core\\lib\\config\\full.js:206:53\n    at cachedFunction (C:\\Video-Platform\\node_modules\\@babel\\core\\lib\\config\\caching.js:40:17)\n    at loadPluginDescriptor (C:\\Video-Platform\\node_modules\\@babel\\core\\lib\\config\\full.js:198:10)\n    at C:\\Video-Platform\\node_modules\\@babel\\core\\lib\\config\\full.js:54:16\n    at Array.map (<anonymous>)\n    at recurseDescriptors (C:\\Video-Platform\\node_modules\\@babel\\core\\lib\\config\\full.js:53:36)\n    at loadFullConfig (C:\\Video-Platform\\node_modules\\@babel\\core\\lib\\config\\full.js:103:6)\n    at C:\\Video-Platform\\node_modules\\@babel\\core\\lib\\transform.js:26:33\n    at _combinedTickCallback (internal/process/next_tick.js:131:7)\n    at process._tickCallback (internal/process/next_tick.js:180:9)");
+
+/***/ }),
+
+/***/ 3:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__("./pages/index.js");
+
+
+/***/ })
+
+},[3])
+          return { page: comp.default }
+        })
+      ;
+//# sourceMappingURL=index.js.map

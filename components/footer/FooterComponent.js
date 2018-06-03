d4ebc1c4c4228fe3826456ab3c5db5ef667e7ea7
PaulJ <jp302119@gmail.com>
2018-06-03 11:46:32 +0900

    컴포넌트 테스트(컴포넌트 폴더 구조 설정)

--- components/footer/FooterComponent.js
+++ components/footer/FooterComponent.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export default class MyPageComponent extends React.Component{
+export default class FooterComponent extends React.Component{
     render() {
         return (
             <div>
-                <input type="button" />
+               This is Footer!
             </div>
         )
     }

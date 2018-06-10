c4984e72b08ff3a982b24739031f0b0ed42c4275
PaulJ <jp302119@gmail.com>
2018-06-10 13:44:01 +0900

    컴포넌트 분리

--- components/header/HeaderComponent.js
+++ components/header/HeaderComponent.js
@@ -1,10 +1,43 @@
 import React from 'react'
+import { Navbar, Nav, NavItem, Container, Grid, Alert } from 'react-bootstrap';
 
 export default class HeaderComponent extends React.Component{
     render() {
         return (
             <div>
-               This is Header!
+            <link
+            rel="stylesheet"
+            href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+            integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+            crossorigin="anonymous"
+          />
+          <Navbar inverse collapseOnSelect>
+            <Navbar.Header>
+              <Navbar.Brand>
+                <a href="#brand">React-Bootstrap</a>
+              </Navbar.Brand>
+              <Navbar.Toggle />
+            </Navbar.Header>
+            <Navbar.Collapse>
+              <Nav>
+                <NavItem eventKey={1} href="#">
+                  Link
+                </NavItem>
+                <NavItem eventKey={2} href="#">
+                  Link
+                </NavItem>
+              </Nav>
+              <Nav pullRight>
+                <NavItem eventKey={1} href="#">
+                  Link Right
+                </NavItem>
+                <NavItem eventKey={2} href="#">
+                  Link Right
+                </NavItem>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+          
             </div>
         )
     }
--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,11 @@
-import React from "react";
-import Link from "next/link";
-import { Navbar, Nav, NavItem, Container, Grid, Alert } from "react-bootstrap";
+import React from 'react';
+import Link from 'next/link';
+import { Grid, Alert } from 'react-bootstrap';
+import HeaderComponent from '../components/header/HeaderComponent';
 
 export default () => (
   <div>
-    <link
-      rel="stylesheet"
-      href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-      integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
-      crossorigin="anonymous"
-    />
-    <Navbar inverse collapseOnSelect>
-      <Navbar.Header>
-        <Navbar.Brand>
-          <a href="#brand">React-Bootstrap</a>
-        </Navbar.Brand>
-        <Navbar.Toggle />
-      </Navbar.Header>
-      <Navbar.Collapse>
-        <Nav>
-          <NavItem eventKey={1} href="#">
-            Link
-          </NavItem>
-          <NavItem eventKey={2} href="#">
-            Link
-          </NavItem>
-        </Nav>
-        <Nav pullRight>
-          <NavItem eventKey={1} href="#">
-            Link Right
-          </NavItem>
-          <NavItem eventKey={2} href="#">
-            Link Right
-          </NavItem>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+    <HeaderComponent />
     <Grid>
       <Alert bsStyle="warning">
         <strong>Holy guacamole!</strong> Best check yo self, you're not looking

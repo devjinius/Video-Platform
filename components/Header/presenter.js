fd450e78fe72053c2b2ff3e047d70386c555d62a
SeonHyungJo <senjing9379@gmail.com>
2018-06-23 23:59:19 +0900

    add view components

--- components/Header/presenter.js
+++ components/Header/presenter.js
@@ -3,34 +3,27 @@ import { Navbar, Nav, NavItem, Container, Grid, Alert } from "react-bootstrap"
 
 const Header = props => {
   return (
-    <>
-      <Navbar inverse collapseOnSelect>
-        <Navbar.Header>
-          <Navbar.Brand>
-            <a href="#brand">React-Bootstrap</a>
-          </Navbar.Brand>
-          <Navbar.Toggle />
-        </Navbar.Header>
-        <Navbar.Collapse>
-          <Nav>
-            <NavItem eventKey={1} href="#">
-              Link
-              </NavItem>
-            <NavItem eventKey={2} href="#">
-              Link
-              </NavItem>
-          </Nav>
-          <Nav pullRight>
-            <NavItem eventKey={1} href="#">
-              Link Right
-              </NavItem>
-            <NavItem eventKey={2} href="#">
-              Link Right
-              </NavItem>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    </>
+    <Navbar inverse collapseOnSelect>
+      <Navbar.Header>
+        <Navbar.Brand>
+          <a href="#brand">React-Bootstrap</a>
+        </Navbar.Brand>
+        <Navbar.Toggle />
+      </Navbar.Header>
+      <Navbar.Collapse>
+        <Nav pullRight>
+          <NavItem eventKey={1} href="#">
+            Home
+          </NavItem>
+          <NavItem eventKey={2} href="#">
+            즐겨찾기
+          </NavItem>
+          <NavItem eventKey={3} href="#">
+            마이페이지
+          </NavItem>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   )
 }
-export default Header;
+export default Header

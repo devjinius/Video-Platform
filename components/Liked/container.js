73ae44a55226ee51f7fb7c0e37c8768fcc6e5dd5
SeonHyungJo <senjing9379@gmail.com>
2018-07-01 00:46:15 +0900

    기본적인 수정완료

--- components/Liked/container.js
+++ components/Liked/container.js
@@ -1,45 +1,9 @@
 import React, { Component } from "react"
 import Liked from "./presenter"
-import * as animationDataA from "./TwitterHeart.json"
-import Lottie from "lottie-web"
 
 class Container extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      isStopped: true,
-      isPaused: false,
-      speed: 1,
-      direction: 1,
-      isLike: false
-    }
-  }
   render() {
-    const centerStyle = {
-      display: "block",
-      margin: "10px auto",
-      textAlign: "center"
-    }
-    const { isStopped, isPaused, direction, speed, isLike } = this.state
-    const defaultOptions = { animationData: animationDataA, loop: false, autoplay: false }
-
-    const clickHandler = () => {
-      if (!isStopped) {
-        this.setState({ direction: direction * -1 })
-      }
-      this.setState({ isStopped: false, isLike: !isLike })
-    }
     return [
-      <Lottie
-        options={defaultOptions}
-        height={400}
-        width={400}
-        isStopped={isStopped}
-        isPaused={isPaused}
-        speed={speed}
-        direction={direction}
-      />,
       <Liked />,
       <link
         rel="stylesheet"
--- components/Subscribe/presenter.js
+++ components/Subscribe/presenter.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { Button } from "react-bootstrap"
-import Lottie from "react-lottie"
 
 const Subscribe = props => {
   return (
@@ -8,13 +7,6 @@ const Subscribe = props => {
       <Button bsStyle="primary" bsSize="large" block>
         구독하기
       </Button>
-      <Lottie
-        options={this.props.defaultOptions}
-        height={400}
-        width={400}
-        isStopped={this.props.isStopped}
-        isPaused={this.props.isPaused}
-      />
     </React.Fragment>
   )
 }

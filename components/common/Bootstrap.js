bfc25562eb8551b87206677166f0d573d87299b4
SeonHyungJo <seonhyung.jo@gmail.com>
2018-07-01 11:10:11 +0900

    공통컴포넌트 생성

---
+++ components/common/Bootstrap.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Bootstrap extends React.Component {
+    render() {
+        return (
+            <link
+                rel="stylesheet"
+                href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+                integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+                crossOrigin="anonymous"
+            />
+        )
+    }
+}
+
+export default Bootstrap;

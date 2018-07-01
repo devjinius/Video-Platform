6994c869abd500104bc032b0f225fa6673c754b7
SeonHyungJo <senjing9379@gmail.com>
2018-07-01 11:05:06 +0900

    bootstrap 공통 컴포넌트 생성

--- components/Explan/container.js
+++ components/Explan/container.js
@@ -1,16 +1,12 @@
 import React, { Component } from "react"
 import Explan from "./presenter"
+import BootStrap from "../common/Bootstrap"
 
 class Container extends Component {
   render() {
     return [
       <Explan />,
-      <link
-        rel="stylesheet"
-        href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-        integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
-        crossOrigin="anonymous"
-      />
+      <BootStrap/>
     ]
   }
 }
--- components/Liked/container.js
+++ components/Liked/container.js
@@ -1,16 +1,12 @@
 import React, { Component } from "react"
 import Liked from "./presenter"
+import BootStrap from "../common/Bootstrap"
 
 class Container extends Component {
   render() {
     return [
       <Liked />,
-      <link
-        rel="stylesheet"
-        href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-        integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
-        crossOrigin="anonymous"
-      />
+      <BootStrap />
     ]
   }
 }

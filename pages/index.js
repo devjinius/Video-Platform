e2cc4abbf45d0b296030478279d055d38c10df44
eugene94 <eugenekang94@gmail.com>
2018-07-15 11:42:28 +0900

    video css 수정

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,18 @@
-import React from "react"
-import { Grid, Alert } from "react-bootstrap"
-import Header from "../components/Header"
-import Liked from "../components/Liked"
-import Subscribe from "../components/Subscribe"
-import Video from "../components/Video"
-import Footer from "../components/Footer"
+import React from 'react';
+import { Grid, Alert } from 'react-bootstrap';
+import Header from '../components/Header';
+import Liked from '../components/Liked';
+import Subscribe from '../components/Subscribe';
+import Video from '../components/Video';
+import Footer from '../components/Footer';
 
 export default () => (
   <>
     <Header />
     <Liked />
-    <Subscribe />
-    <Video />
+    <Grid>
+      <Video />
+    </Grid>
     <Footer />
   </>
-)
+);

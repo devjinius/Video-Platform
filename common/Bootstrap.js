90e890d5f87cc0acd55a96cd9dcd287f1a3ab21f
SeonHyungJo <senjing9379@gmail.com>
2018-07-17 23:53:09 +0900

    공통파일 이동 진행

---
+++ common/Bootstrap.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+class Bootstrap extends React.Component {
+  render() {
+    return <link rel="stylesheet" href="./bootstrap.min.css" />
+  }
+}
+
+export default Bootstrap

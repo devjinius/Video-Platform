9cf49167c1e50ec001fbfe53f361332f7a0dd9d9
eugene94 <eugenekang94@gmail.com>
2018-07-22 12:58:35 +0900

    sticky top - body padding 추가

--- components/PageTemplate/PageTemplate.js
+++ components/PageTemplate/PageTemplate.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import Header from '../Header/';
 import Navigator from '../Navigator';
-import Footer from "../Footer";
+import Footer from '../Footer';
 
 export default ({ children }) => {
   return (
     <>
-    <Header />
-    <Navigator />
-    {children}
-    <Footer />
+      <Header />
+      <Navigator />
+      {children}
+      <Footer />
+      <style jsx>
+        {' '}
+        {`
+          body {
+            padding-top: 60px;
+          }
+        `}{' '}
+      </style>
     </>
   );
 };
